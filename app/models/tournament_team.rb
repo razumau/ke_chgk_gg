4a4fee4d411a999dce2216e9a2f82de53d7afc64
Jury Razumau <jury.razumau@zendesk.com>
2021-03-21 17:56:55 +0100

    add chgk results on the tournament page

--- app/models/tournament_team.rb
+++ app/models/tournament_team.rb
@@ -4,6 +4,7 @@ class TournamentTeam < ApplicationRecord
   belongs_to :team
   has_many :teamtournamentplayers, class_name: "TeamTournamentPlayer", foreign_key: "team_tournament_id"
   has_many :players, through: :teamtournamentplayers
+  has_one :chgkresult, class_name: "ChgkResult", foreign_key: "team_tournament_id"
 
   def rating_link
     "https://rating.chgk.info/team/#{rating_id}"
