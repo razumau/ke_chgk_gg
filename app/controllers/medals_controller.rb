234a8ba8c80c14305508d3495e974c3c456ad15e
Jury Razumau <jury.razumau@zendesk.com>
2021-04-02 12:05:45 +0200

    sort medals by gold first

--- app/controllers/medals_controller.rb
+++ app/controllers/medals_controller.rb
@@ -1,6 +1,6 @@
 class MedalsController < ApplicationController
   def index
     @medals = Medal.where("total > 0")
-                   .order("total DESC, gold DESC, silver DESC")
+                   .order("gold DESC, silver DESC, bronze DESC")
   end
 end
